411962b58deca93045a807b2402a837335557f3f
Ted John <ted@brambles.org>
2017-05-06 22:48:55 +0100

    Fix Linux build errors

--- src/openrct2/platform/posix.c
+++ src/openrct2/platform/posix.c
@@ -46,24 +46,6 @@
 utf8 _userDataDirectoryPath[MAX_PATH] = { 0 };
 utf8 _openrctDataDirectoryPath[MAX_PATH] = { 0 };
 
-/**
- * The function that is called directly from the host application (rct2.exe)'s WinMain.
- * This will be removed when OpenRCT2 can be built as a stand alone application.
- */
-sint32 main(sint32 argc, const char **argv)
-{
-	core_init();
-
-	sint32 run_game = cmdline_run(argv, argc);
-	if (run_game == 1)
-	{
-		openrct2_launch();
-	}
-
-	exit(gExitCode);
-	return gExitCode;
-}
-
 void platform_get_date_utc(rct2_date *out_date)
 {
 	assert(out_date != NULL);
